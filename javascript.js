c679393c75eb9c15a3e1176575b3706d0136ecb8
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-04 13:57:24 -0600

    Updated Javascript

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function passInfo() {
+    let numChar = prompt("How many characters do you want?");
+    console.log(numChar);
+    let upperCase = confirm("Do you want UPPER CASE letters?");
+    console.log(upperCase);
+    let lowerCase = confirm("Do you want lower case letters?");
+    console.log(lowerCase);
+    let ranNum = confirm("Do you want numbers?");
+    console.log(ranNum);
+    let specChar = confirm("Do you want any special characters?");  
+    console.log(specChar);  
+}
