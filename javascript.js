37b1ff5f5a8542cf5deeba976583d162689f5ad6
Phillip Jones <phillipjones@Amandas-MBP.attlocal.net>
2019-11-04 20:23:14 -0600

    Added Arrays

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
-function passInfo() {
-    let numChar = prompt("How many characters do you want?");
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "Y", "U", "V", "W", "X", "Y", "Z"];
+let ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specChar = ["!", "#", "$", "%", "&", "*", "+", "/", "<", "=", ">", "?", "@", "^", "_", "~"];
+
+function ranPass() {
+    let numChar = prompt("How many characters do you want? (min of 8)");
     console.log(numChar);
-    let upperCase = confirm("Do you want UPPER CASE letters?");
-    console.log(upperCase);
-    let lowerCase = confirm("Do you want lower case letters?");
-    console.log(lowerCase);
-    let ranNum = confirm("Do you want numbers?");
-    console.log(ranNum);
-    let specChar = confirm("Do you want any special characters?");  
-    console.log(specChar);  
+    let upperUser = confirm("Do you want UPPER CASE letters?");
+    console.log(upperUser);
+    let lowerUser = confirm("Do you want lower case letters?");
+    console.log(lowerUser);
+    let ranUserNum = confirm("Do you want numbers?");
+    console.log(ranUserNum);
+    let specCharUser = confirm("Do you want any special characters?");  
+    console.log(specCharUser);  
+
+    
 }
