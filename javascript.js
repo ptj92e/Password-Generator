cd383be0e06d25f4fb3e51c49322ae07d44c9e1c
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-06 07:31:08 -0600

    Added Copy Function

--- javascript.js
+++ javascript.js
@@ -33,4 +33,11 @@ function ranPass() {
     document.getElementById("password").innerHTML = password;
 }
 
-
+function copyClip() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
+  
