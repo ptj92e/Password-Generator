20e05d49d020cf5a076645f6957452c891e2b812
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-05 20:13:35 -0600

    Added Rewrite

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "Y", "U", "V", "W", "X", "Y", "Z"];
-let ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let specChar = ["!", "#", "$", "%", "&", "*", "+", "/", "<", "=", ">", "?", "@", "^", "_", "~"];
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "Y", "U", "V", "W", "X", "Y", "Z",];
+let ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+let specChar = ["!", "#", "$", "%", "&", "*", "+", "/", "<", "=", ">", "?", "@", "^", "_", "~",];
 let passwordArray = [""];
 let password = [""];
 
@@ -14,20 +14,22 @@ function ranPass() {
     let specCharUser = confirm("Do you want any special characters?");    
 
     if (upperUser === true) {
-     password = passwordArray + upperCase;
+     passwordArray = passwordArray + upperCase;
     } 
     if (lowerUser === true) {
-     password = passwordArray + lowerCase;
+     passwordArray = passwordArray + lowerCase;
     }
     if (ranUserNum === true) {
-     password = passwordArray + ranNum;
+     passwordArray = passwordArray + ranNum;
     }
     if (specCharUser === true) {
-     password = passwordArray + specChar;
+     passwordArray = passwordArray + specChar;
     }
 
     for (i = 0; i <= userNum; i++) {
-       let password = Math.floor(Math.random(password[i]));
+       password = Math.floor(Math.random(passwordArray));
         console.log(password);
     }
+
+    document.getElementById("password").innerHTML = password;
 }
