0e3e345eac812abb7259d6a42a45faaff344c743
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-05 20:53:10 -0600

    Solved issues with generator

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "Y", "U", "V", "W", "X", "Y", "Z",];
-let ranNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-let specChar = ["!", "#", "$", "%", "&", "*", "+", "/", "<", "=", ">", "?", "@", "^", "_", "~",];
+let lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+let upperCase = ["ABCDEFGHIJKLMNOPQRSYUVWXYZ"];
+let ranNum = ["0123456789"];
+let specChar = ["!", "#$%&*+/<=>?@^_~"];
 let passwordArray = [""];
 let password = [""];
 
@@ -26,10 +26,11 @@ function ranPass() {
      passwordArray = passwordArray + specChar;
     }
 
-    for (i = 0; i <= userNum; i++) {
-       password = Math.floor(Math.random(passwordArray));
-        console.log(password);
+    for (i = 0; i < userNum; i++) {
+       password += passwordArray[Math.floor(Math.random() * passwordArray.length)];
     }
 
     document.getElementById("password").innerHTML = password;
 }
+
+
